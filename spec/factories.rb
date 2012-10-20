b9953aad752e62a1fdef5a0f86bc0f439d3231da
gantzinis <gantzinis@gmail.com>
2012-10-20 23:58:41 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    
+    name "michalis oikonomou"
+    email "gantzinis@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
