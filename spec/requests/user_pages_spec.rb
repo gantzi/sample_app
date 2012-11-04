4345791387667ac4d826a55d7807003edbea648c
gantzinis <gantzinis@gmail.com>
2012-11-04 18:18:07 +0200

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,14 +22,18 @@ describe "signup page" do
    it { should have_selector('title', text: full_title('Sign up')) }
    
     end
-      describe "signup" do
+    
+    
+describe "signup" do
 
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
+
+
+describe "with invalid information" do
+     it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
@@ -46,8 +50,14 @@ describe "signup page" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
+    
+describe "after saving the user" do
+    before { click_button submit }
+    let(:user) { User.find_by_email('user@example.com') } 
+    
+    it { should have_selector('title', text: user.name) }
+    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    it { should have_link('Sign out') }
   end
-    
-    
-   end
 
+end
